d16be24f1e2aec215add1896d03a1890d8493989
tgaudz <tylergaudino@gmail.com>
2026-04-19 20:16:37 -0400

    Update streamlit-dsc333.py

--- streamlit-dsc333.py
+++ streamlit-dsc333.py
@@ -3,3 +3,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+
+
+st.title("Measuring Foot Traffic at Lansing Building Products")
+st.subheader("A Data Science Project by Tyler Gaudino and Asante Frye")
+url_lansing = "https://leaptodigital.com/partner/lansing-building-products/"
+
+st.image(url_lansing, use_container_width=True)
